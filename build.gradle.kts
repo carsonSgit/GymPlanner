1144a62713dae3d05b9177aa09763289e8462391
Noah Groleau <98351050+NoahGJAC@users.noreply.github.com>
2023-10-06 16:23:45 -0400

    KTLint

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,16 @@
 plugins {
     id("com.android.application") version "8.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
+}
+
+ktlint {
+    verbose.set(true)
+    outputToConsole.set(true)
+    coloredOutput.set(true)
+    filter {
+        exclude("**/ExampleInstrumentedTest.kt")
+        exclude("**/settings.gradle.kts")
+        exclude("**/ktlintKotlinScriptCheck.txt")
+    }
 }
